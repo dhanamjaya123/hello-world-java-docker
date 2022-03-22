bba4c9aa113901ab3dd85300840433f26b74dd83
dhanamjaya123 <dhanu8121@gmail.com>
2022-03-22 16:13:56 +0530

    Update IndexController.java

--- src/main/java/com/edw/controller/IndexController.java
+++ src/main/java/com/edw/controller/IndexController.java
@@ -1,24 +1,18 @@
 package com.edw.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-
+ 
+@RefreshScope
 @RestController
 public class IndexController {
-    private Logger logger = LoggerFactory.getLogger(IndexController.class);
-
+    @Value("${name:Somewhat Default}")
+    private String message;
+ 
     @GetMapping("/")
-    public HashMap index() {
-        logger.debug("request served");
-
-        return new HashMap() {{
-            put("success", true);
-            put("hello", "world");
-            put("new-message", "adding a new msg");
-        }};
+    public String getMessage() {
+        return this.message;
     }
 }
